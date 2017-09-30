b9cbd345219f3f39c53d8f15bb05544141d7d946
Ygor Bruxel <darkmigore@gmail.com>
2017-09-30 14:56:09 +0200

    #2 Add Service annotation to statistics calculator

--- src/main/java/com/statistics/services/StatisticsCalculator.java
+++ src/main/java/com/statistics/services/StatisticsCalculator.java
@@ -2,10 +2,12 @@ package com.statistics.services;
 
 import com.statistics.domain.Statistics;
 import com.statistics.domain.Transaction;
+import org.springframework.stereotype.Service;
 
 import java.util.DoubleSummaryStatistics;
 import java.util.List;
 
+@Service
 public class StatisticsCalculator {
     public Statistics calculate(List<Transaction> all) {
         DoubleSummaryStatistics summaryStatistics = all
