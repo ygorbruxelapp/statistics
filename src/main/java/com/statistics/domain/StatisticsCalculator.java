0815be142667b5e996f8dddd441cd438bc92a22b
Ygor Bruxel <darkmigore@gmail.com>
2017-09-30 14:51:05 +0200

    #2 Use java summary statistics to calculate

--- src/main/java/com/statistics/domain/StatisticsCalculator.java
+++ src/main/java/com/statistics/domain/StatisticsCalculator.java
@@ -1,21 +1,21 @@
 package com.statistics.domain;
 
+import java.util.DoubleSummaryStatistics;
 import java.util.List;
 
 public class StatisticsCalculator {
     public Statistics calculate(List<Transaction> all) {
-        double sum = all.stream().mapToDouble(Transaction::getAmount).sum();
-        double avg = all.stream().mapToDouble(Transaction::getAmount).average().getAsDouble();
-        double max = all.stream().mapToDouble(Transaction::getAmount).max().getAsDouble();
-        double min = all.stream().mapToDouble(Transaction::getAmount).min().getAsDouble();
-        long count = all.size();
+        DoubleSummaryStatistics summaryStatistics = all
+                .stream()
+                .mapToDouble(Transaction::getAmount)
+                .summaryStatistics();
 
         Statistics statistics = new Statistics();
-        statistics.setSum(sum);
-        statistics.setAvg(avg);
-        statistics.setMax(max);
-        statistics.setMin(min);
-        statistics.setCount(count);
+        statistics.setSum(summaryStatistics.getSum());
+        statistics.setAvg(summaryStatistics.getAverage());
+        statistics.setMax(summaryStatistics.getMax());
+        statistics.setMin(summaryStatistics.getMin());
+        statistics.setCount(summaryStatistics.getCount());
 
         return statistics;
     }
