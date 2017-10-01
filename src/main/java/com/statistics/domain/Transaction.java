57e35026d409ae8b1a170aa1a5516aeab5c8ee4f
Ygor Bruxel <darkmigore@gmail.com>
2017-10-01 09:57:03 +0200

    #6 Extract constants

--- src/main/java/com/statistics/domain/Transaction.java
+++ src/main/java/com/statistics/domain/Transaction.java
@@ -5,14 +5,16 @@ import java.util.UUID;
 
 public class Transaction {
 
+    private static final int SECONDS_TO_EXPIRE = 60;
+    public static final int TO_MILLISECONDS = 1000;
     private double amount;
     private long timestamp;
     private UUID id;
 
     public boolean isExpired(ZonedDateTime utc) {
-        ZonedDateTime sixtySecondsAgo = utc.minusSeconds(60);
+        ZonedDateTime sixtySecondsAgo = utc.minusSeconds(SECONDS_TO_EXPIRE);
 
-        return timestamp < (sixtySecondsAgo.toEpochSecond() * 1000);
+        return timestamp < (sixtySecondsAgo.toEpochSecond() * TO_MILLISECONDS);
     }
 
     public long getTimestamp() {
